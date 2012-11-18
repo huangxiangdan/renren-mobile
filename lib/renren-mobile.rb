01d4caa30e53438bef8ca9c6ce35db05674c88d8
Danny <xd_huang1986@163.com>
2012-11-18 14:19:38 +0800

    first commit

---
+++ lib/renren-mobile.rb
@@ -0,0 +1,8 @@
+# code is an adaptation of the twitter gem by John Nunemaker
+# http://github.com/jnunemaker/twitter
+# Copyright (c) 2009 John Nunemaker
+#
+# made to work with china's facebook, 人人网
+
+require 'renren-mobile/config'
+require 'renren-mobile/base'
---
+++ lib/renren-mobile/base.rb
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+require 'uri'
+require 'multi_json'
+require 'net/http'
+require 'digest'
+require 'rest_client'
+
+module RenrenMobile
+  class Base
+    attr_accessor :params
+    
+    def initialize(access_token)
+      @params = {}
+      @params[:call_id] = Time.now.to_i
+      @params[:format] = 'json'
+      @params[:v] = '1.0'
+      @params[:access_token] = access_token
+    end
+    
+    def call_method(method = nil, opts = {})
+      unless method
+        method = opts[:method]
+        opts.delete(:method)
+      end
+      method = method.gsub(".", "/")
+      MultiJson.decode(Net::HTTP.post_form(URI.parse("http://api.m.renren.com/api/#{method}"), update_params(opts)).body)
+    end
+    
+    def upload_file(method = nil, filename, opts = {})
+      unless method
+        method = opts[:method]
+        opts.delete(:method)
+      end
+      method = method.gsub(".", "/")
+      MultiJson.decode(RestClient.post('http://api.renren.com/restserver.do', update_params(opts).merge(:data => File.new(filename))))
+    end
+    
+    private
+      def update_params(opts)
+        params = @params.merge(opts){|key, first, second| second}
+        params[:sig] = Digest::MD5.hexdigest(params.map{|k,v| "#{k}=#{v}"}.sort.join + Config.api_secret)
+        params
+      end
+  end
+end
